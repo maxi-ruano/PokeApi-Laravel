77ad61944228a7944787e6bfe100c4c9f766c4ee
maxi-ruano <ruanomaxi@gmail.com>
2024-02-09 10:43:07 -0300

    New-Commit

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -4,32 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Pagination\LengthAwarePaginator;
+
 
 class PokemonController extends Controller
 {
 
-
-
-
-    public function index()
+public function index()
     {
-        $response = Http::get('https://pokeapi.co/api/v2/pokemon');
-        $pokemons = $response->json()['results'];
-
-        foreach ($pokemons as &$pokemon) {
-            $detailsResponse = Http::get($pokemon['url']);
-            $details = $detailsResponse->json();
-            $image_url = data_get($details, 'sprites.front_default', null);
-
-            if ($image_url) {
+        $cachedData = Cache::get('pokemon_data');
+    
+        if ($cachedData) {
+            $pokemons = $cachedData;
+        } else {
+            $apiUrl = 'https://pokeapi.co/api/v2/pokemon';
+            $response = file_get_contents($apiUrl);
+            $data = json_decode($response, true);
+    
+            $pokemons = $data['results'];
+    
+            foreach ($pokemons as &$pokemon) {
+                $detailsResponse = file_get_contents($pokemon['url']);
+                $details = json_decode($detailsResponse, true);
+                $image_url = data_get($details, 'sprites.front_default', null);
+    
                 $pokemon['image_url'] = $image_url;
-            } else {
-                $pokemon['image_url'] = 'URL_DE_IMAGEN_DE_REEMPLAZO_O_BLANCO';
+    
+                $pokemon['id'] = $details['id'];
             }
-
-            $pokemon['id'] = $details['id'];
+    
+            Cache::put('pokemon_data', $pokemons, 3600);
         }
-
         $searchTerm = request('name');
         if ($searchTerm) {
             $pokemons = array_filter($pokemons, function ($pokemon) use ($searchTerm) {
@@ -37,24 +43,25 @@ class PokemonController extends Controller
             });
         }
 
-        $collection = collect($pokemons);
 
         $perPage = 10;
         $currentPage = request('page', 1);
-
-        $paginatedResults = new \Illuminate\Pagination\LengthAwarePaginator(
-            $collection->forPage($currentPage, $perPage),
-            $collection->count(),
+        $totalItems = count($pokemons);
+        $offset = ($currentPage - 1) * $perPage;
+    
+        $paginatedResults = array_slice($pokemons, $offset, $perPage);
+    
+        $paginatedResults = new LengthAwarePaginator(
+            $paginatedResults,
+            $totalItems,
             $perPage,
             $currentPage,
             ['path' => url('/'), 'query' => request()->query()]
         );
         $searchResults = !empty(request('name')) ? $paginatedResults->items() : null;
 
-        return view('pokemons.index', compact('paginatedResults', 'searchResults'));
+        return view('pokemons.index', compact('paginatedResults','searchResults'));
     }
-
-
 
 
 
